0aa91aa44bbb8fede48adc9f5da9b98570da6bbe
Kosala Tennakoon <kosala.me@gmail.com>
2022-12-29 11:46:22 +0530

    added count to paginator

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/kosatnkn/req
 
-go 1.15
+go 1.18
--- paginator/Paginator.go
+++ paginator/Paginator.go
@@ -2,6 +2,7 @@ package paginator
 
 // Paginator entity
 type Paginator struct {
-	Page uint32
-	Size uint32
+	Page  uint32
+	Size  uint32
+	Count uint32
 }
